16976ff590840d6e7977936654da720b954117df
Bruno Germano <bruno.germano@gmail.com>
2014-05-02 21:51:39 -0300

    update footer embeds

--- app/footer.php
+++ app/footer.php
@@ -19,7 +19,38 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+
+<!-- build:js scripts/vendor.js -->
+<!-- bower:js -->
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/lodash/dist/lodash.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/jquery/jquery.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/bower_components/chosen/chosen.jquery.min.js" ></script>
+<!-- endbower -->
+<!-- endbuild -->
+
+<!-- build:js scripts/main.js -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+<!-- endbuild -->
+
+<script>
+    var CONFIG = {
+        templateUrl: '<?php echo get_template_directory_uri(); ?>',
+        baseUrl: '<?php echo site_url(); ?>',
+    };
+</script>
+
 <?php wp_footer(); ?>
+
+
 
 </body>
 </html>
